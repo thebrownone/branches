fea649d82ae149ab18a1127cd54701af107b2c04
thebrownone <97177514+thebrownone@users.noreply.github.com>
2023-02-20 19:33:54 -0500

    Rust Control Flow practice

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 8;
 
-    /// generate library docs 
-    /// 
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    // Because if is an expression,
+    // we can use it on the right side of a let statement to assign the outcome to a variable
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+
+    // below code will not compile
+    let x = 1;
+    let y = if x { 0 } else { 1 }; 
+    println!("{y}");
+    //The condition to an if-expression must be a boolean. Rust does not have a concept of "truthy" or "falsy" values.
+    // The if expression is not allowed to return a value of different types 
 }
